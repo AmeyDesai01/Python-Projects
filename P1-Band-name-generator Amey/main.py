e46917e5377fdb728a48072e00149f53b7635656
Amey Eknath Desai <101491599+AmeyDesai01@users.noreply.github.com>
2022-03-14 12:31:40 +0530

    Project using Variables in Python

---
+++ P1-Band-name-generator Amey/main.py
@@ -0,0 +1,6 @@
+print("Welcome to the Band Name Generator.")
+street = input("What's name of the city you grew up in?\n")
+describe = input("Which word describes your band?\n") 
+#It could be Cool,mesmerise or party animals
+
+print("Your band name could be " + street + " " + describe)
